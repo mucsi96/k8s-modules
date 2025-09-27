ad2951ca6760978d2edad16c0a9ed552075bf995
Igor Bari <mucsi96@gmail.com>
2025-09-27 14:44:49 +0200

    remove AI service module and related configurations

--- modules/setup_ai/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "The name of the Azure Resource Group"
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure location to deploy resources"
-  type        = string
-}
